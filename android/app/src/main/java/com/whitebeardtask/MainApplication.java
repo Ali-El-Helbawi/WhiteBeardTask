21f3a42799d63827d362bdc007dd4c458450a601
Ali El Helbawi <ali.al.helbawi.2017@gmail.com>
2022-05-03 01:16:34 +0300

    fix ssl android

--- android/app/src/main/java/com/whitebeardtask/MainApplication.java
+++ android/app/src/main/java/com/whitebeardtask/MainApplication.java
@@ -14,6 +14,7 @@ import com.whitebeardtask.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -90,4 +91,12 @@ public class MainApplication extends Application implements ReactApplication {
       }
     }
   }
+
+
+
+
+
+
+
+
 }
